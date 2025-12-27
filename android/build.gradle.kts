ff16b26a2d8713bdeb521f97c9162253c56639f4
yuvraj-aura <xtremeaura0110@gmail.com>
2025-12-27 19:59:40 +0530

    Align build environment to Flutter 3.19

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,19 +16,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-subprojects {
-    afterEvaluate {
-        val android = extensions.findByName("android")
-        if (android != null) {
-             val namespaceProp = android.javaClass.getMethod("getNamespace")
-             if (namespaceProp.invoke(android) == null) {
-                 val setNamespaceStart = android.javaClass.getMethod("setNamespace", String::class.java)
-                 setNamespaceStart.invoke(android, "com.fluxnotes.${project.name.replace("-", "_")}")
-             }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
